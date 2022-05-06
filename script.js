774a985d783e5b9c439ab55c0d55be3844a9276c
KraZyK-on <82599388+KraZyK-on@users.noreply.github.com>
2022-05-06 20:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function validate(){
     var email=document.getElementById("email").value;
 
     if (username=="/^[a-zA-Z0-9]+$/" && email=="/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/" && password=="/^[a-zA-Z0-9]+$/"){
-        window.location.href("https://login-l17lkwtpr-karanbisht1915-gmailcom.vercel.app/")
+        window.location.href("https://login-three-tawny.vercel.app/")
     }
     else{
         alert("Invalid format");
