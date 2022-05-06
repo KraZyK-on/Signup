761dea12694f96ec071b636e59c8c0b9e64fcbf0
KraZyK-on <82599388+KraZyK-on@users.noreply.github.com>
2022-05-06 19:32:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validate(){
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    var email=document.getElementById("email").value;
+
+    if (username=="/^[a-zA-Z0-9]+$/" && email=="/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/" && password=="/^[a-zA-Z0-9]+$/"){
+        window.location.href("https://login-l17lkwtpr-karanbisht1915-gmailcom.vercel.app/")
+    }
+    else{
+        alert("Invalid format");
+    }
+}
