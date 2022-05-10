dbbd3f05f076f58bca598fdce26ed4b0eed0b182
KraZyK-on <82599388+KraZyK-on@users.noreply.github.com>
2022-05-10 18:05:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function validate(){
-    var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
     var email=document.getElementById("email").value;
+    var password_val = /^[A-Za-z]+$/;
+    var email_val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-    if (username=="/^[a-zA-Z0-9]+$/" && email=="/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/" && password=="/^[a-zA-Z0-9]+$/"){
-        window.location.href("https://login-three-tawny.vercel.app/")
+    if (email=='' && password=='') {
+        alert('Please enter details')
+    }
+    else if(!email_val.test(email)){
+        alert('Please enter valid format')
+    }
+    else if(!password_val.test(password)){
+        alert('Please enter valid password')
     }
     else{
-        alert("Invalid format");
+        window.location.href="https://login-three-tawny.vercel.app/"
     }
 }
